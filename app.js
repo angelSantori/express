530cd707e9d841fda28c919dc66cb9ad461ba6d1
angelSantori <antonyo.da.mm@gmail.com>
2023-11-07 09:59:46 -0700

    Diferentes rutas

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hola Mundo!');
+    res.send('Mi primera appi');
+});
+
+app.get('/saludo', function (req, res) {
+    res.send('Hola mundo');
+});
+
+app.get('/despedida', function (req, res) {
+    res.send('Adios mundo cruel');
+});
+
+app.get('/despedida/familia', function (req, res) {
+    res.send('Les quiero mucho a todos');
+});
+
+app.get('/despedida/empleados', function (req, res) {
+    res.send('Ojala se rompan una pierna');
 });
 
 app.listen(3000, function() {
