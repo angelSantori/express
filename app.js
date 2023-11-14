b3eb8a6e242984f80c65c66b251084e3274f2067
angelSantori <antonyo.da.mm@gmail.com>
2023-11-14 10:14:47 -0700

    App funcional

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-var express = require('express');
-var app = express();
+//Requiere packages and set the port
+const express = require('express');
+const port = 3002;
 
-app.get('/', function (req, res) {
-    res.send('Mi primera appi');
+//Para emitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+//Usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+//Iniciar el servidor
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+        
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
-
-app.get('/saludo', function (req, res) {
-    res.send('Hola mundo');
-});
-
-app.get('/despedida', function (req, res) {
-    res.send('Adios mundo cruel');
-});
-
-app.get('/despedida/familia', function (req, res) {
-    res.send('Les quiero mucho a todos');
-});
-
-app.get('/despedida/empleados', function (req, res) {
-    res.send('Ojala se rompan una pierna');
-});
-
-app.listen(3000, function() {
-    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
-});
-
